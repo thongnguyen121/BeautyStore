e6f82216675a43f3b74cf6b8dc6904b2eb54b952
thongnguyen121 <thongnguyen12102002@gmail.com>
2023-10-23 10:32:34 +0700

    Đăng nhập, đăng kí và kiểm tra quyền khi đăng nhập

---
+++ app/src/main/java/com/example/beautystore/SignUpActivity.java
@@ -0,0 +1,179 @@
+package com.example.beautystore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.beautystore.model.Customer;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+    Button btnBack, btnSignUp;
+    EditText edtTen, edtMail, edtSDT, edtPass;
+    ImageView ivShowHidePass;
+    String name, email, phoneNum, pass;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        setControl();
+        setEvent();
+    }
+
+    private void setEvent() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        ivShowHidePass.setImageResource(R.drawable.hide);
+        ivShowHidePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtPass.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    show(edtPass, ivShowHidePass);
+                    edtPass.setSelection(edtPass.getText().length());
+                }
+                else{
+                    hide(edtPass, ivShowHidePass);
+                    edtPass.setSelection(edtPass.getText().length());
+                }
+            }
+        });
+        checkEditText();
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Signup();
+            }
+        });
+    }
+
+    private void checkEditText() {
+
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+             name = edtTen.getText().toString();
+             email = edtMail.getText().toString();
+             phoneNum = edtSDT.getText().toString();
+             pass = edtPass.getText().toString();
+            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(phoneNum) || TextUtils.isEmpty(pass)) {
+                // Nếu một trong các trường văn bản trống, disable nút đăng kí
+                btnSignUp.setEnabled(false);
+                btnSignUp.setBackgroundResource(R.drawable.btn_login_bg_mt); // Thay đổi background màu xám (ví dụ)
+            } else {
+                // Nếu tất cả các trường đã được điền đầy đủ, enable nút đăng kí
+                btnSignUp.setEnabled(true);
+                btnSignUp.setBackgroundResource(R.drawable.btn_login_bg); // Thay đổi background màu mặc định
+            }
+        }
+    };
+        edtTen.addTextChangedListener(textWatcher);
+        edtMail.addTextChangedListener(textWatcher);
+        edtSDT.addTextChangedListener(textWatcher);
+        edtPass.addTextChangedListener(textWatcher);
+    }
+
+    private void Signup() {
+        name = edtTen.getText().toString();
+        email = edtMail.getText().toString();
+        phoneNum = edtSDT.getText().toString();
+        pass = edtPass.getText().toString();
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edtMail.setError("Vui lòng cung cấp đúng email!");
+            edtMail.requestFocus();
+            return;
+        }
+        if (pass.length() < 6) {
+            edtPass.setError("Độ dài tối thiểu của mật khẩu là 6");
+            edtPass.requestFocus();
+            return;
+        }
+        if (phoneNum.length() < 10){
+            edtSDT.setError("Độ dài tối thiểu của số điện thoại là 8");
+            edtSDT.requestFocus();
+            return;
+        }if (phoneNum.length() >10){
+            edtSDT.setError("Độ dài tối đa của số điện thoại là 10");
+            edtSDT.requestFocus();
+            return;
+        }
+        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Customer customer = new Customer(name,email,pass,phoneNum,"","","");
+                    databaseReference.child("Customer").child(firebaseAuth.getCurrentUser().getUid()).setValue(customer).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this, "Thành công", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(SignUpActivity.this, "Không thanh công", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void hide(EditText edtPass, ImageView ivShowHidePass) {
+        edtPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+        ivShowHidePass.setImageResource(R.drawable.view);
+    }
+
+    private void show(EditText edtPass, ImageView ivShowHidePass) {
+        edtPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        ivShowHidePass.setImageResource(R.drawable.hide);
+    }
+
+    private void setControl() {
+        btnBack = findViewById(R.id.btnBack);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        edtMail = findViewById(R.id.edtEmailDK);
+        edtTen = findViewById(R.id.edtFullname);
+        edtSDT = findViewById(R.id.edtPhoneNum);
+        edtPass = findViewById(R.id.edtPassDK);
+        ivShowHidePass = findViewById(R.id.ivShowHideSignupPass);
+    }
+}
---
+++ app/src/main/java/com/example/beautystore/model/Employees.java
@@ -0,0 +1,92 @@
+package com.example.beautystore.model;
+
+public class Employees {
+    String username, email, password, phoneNumber, profileImage, hinhCCCDMatTruoc, hinhCCCDMatSau, status, role;
+
+    public Employees(String username, String email, String password, String phoneNumber, String profileImage, String hinhCCCDMatTruoc, String hinhCCCDMatSau, String status, String role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.profileImage = profileImage;
+        this.hinhCCCDMatTruoc = hinhCCCDMatTruoc;
+        this.hinhCCCDMatSau = hinhCCCDMatSau;
+        this.status = status;
+        this.role = role;
+    }
+
+    public Employees() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getHinhCCCDMatTruoc() {
+        return hinhCCCDMatTruoc;
+    }
+
+    public void setHinhCCCDMatTruoc(String hinhCCCDMatTruoc) {
+        this.hinhCCCDMatTruoc = hinhCCCDMatTruoc;
+    }
+
+    public String getHinhCCCDMatSau() {
+        return hinhCCCDMatSau;
+    }
+
+    public void setHinhCCCDMatSau(String hinhCCCDMatSau) {
+        this.hinhCCCDMatSau = hinhCCCDMatSau;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
