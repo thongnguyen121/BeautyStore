a989b82328be3c716ddd230c4f19e40a0032addd
thongnguyen121 <thongnguyen12102002@gmail.com>
2023-10-23 10:37:56 +0700

    Sửa lỗi

--- app/src/main/java/com/example/beautystore/LoginActivity.java
+++ app/src/main/java/com/example/beautystore/LoginActivity.java
@@ -88,6 +88,7 @@ public class LoginActivity extends AppCompatActivity {
                                         Toast.makeText(LoginActivity.this, "Khach", Toast.LENGTH_SHORT).show();
                                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                         startActivity(intent);
+                                        finish();
                                     } else {
                                         Toast.makeText(LoginActivity.this, "Khong phai khach", Toast.LENGTH_SHORT).show();
 //                                        checkRole(uid);
