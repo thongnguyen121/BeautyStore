bfa04a585702dcb9793932a44839fc7a57461c0b
quynhanh0298 <quynhanh0298.com>
2023-11-16 13:01:07 +0700

    update add to wishlist feature

--- app/src/main/java/com/example/beautystore/adapter/RecyclerView_WishList.java
+++ app/src/main/java/com/example/beautystore/adapter/RecyclerView_WishList.java
@@ -1,6 +1,7 @@
 package com.example.beautystore.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.beautystore.R;
+import com.example.beautystore.activity.Activity_Product_Detail;
 import com.example.beautystore.activity.Activity_Wish_List;
 import com.example.beautystore.model.CartDetail;
 import com.example.beautystore.model.Products;
@@ -50,6 +52,7 @@ public class RecyclerView_WishList extends RecyclerView.Adapter<RecyclerView_Wis
         String productId = wishList.getProduct_id();
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference().child("Products");
+        DatabaseReference wishlistRef = firebaseDatabase.getReference().child("WishList");
         databaseReference.child(productId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -61,6 +64,20 @@ public class RecyclerView_WishList extends RecyclerView.Adapter<RecyclerView_Wis
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Activity_Product_Detail.class);
+                intent.putExtra("products_id", productId);
+                context.startActivity(intent);
+            }
+        });
+        holder.ivAddWishList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wishlistRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(productId).removeValue();
             }
         });
     }
