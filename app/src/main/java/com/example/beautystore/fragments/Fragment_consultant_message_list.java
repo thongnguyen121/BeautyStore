c10fae51df170f9ba82df1748aeb980e35657921
quynhanh0298 <quynhanh0298.com>
2023-11-16 23:10:36 +0700

    fix chat feature

--- app/src/main/java/com/example/beautystore/fragments/Fragment_consultant_message_list.java
+++ app/src/main/java/com/example/beautystore/fragments/Fragment_consultant_message_list.java
@@ -14,7 +14,7 @@ import android.view.ViewGroup;
 import com.example.beautystore.R;
 import com.example.beautystore.adapter.RecyclerView_Consultant_Message;
 import com.example.beautystore.adapter.RecyclerView_WishList;
-import com.example.beautystore.model.ChatList;
+import com.example.beautystore.model.ChatGroup;
 import com.example.beautystore.model.WishList;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,7 +31,7 @@ public class Fragment_consultant_message_list extends Fragment {
     DatabaseReference databaseReference;
     RecyclerView messageRecyclerView;
     RecyclerView_Consultant_Message messageAdapter;
-    ArrayList<String> chatsList;
+    ArrayList<ChatGroup> chatGroups;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,13 +40,32 @@ public class Fragment_consultant_message_list extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_consultant_message_list, container, false);
         messageRecyclerView = view.findViewById(R.id.rvConsultantMessageList);
 
-//        chatsList = new ArrayList<>();
-        messageAdapter = new RecyclerView_Consultant_Message(getContext());
+        chatGroups = new ArrayList<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference("ChatGroup");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    chatGroups.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        ChatGroup chatGroup = snapshot.getValue(ChatGroup.class);
+                        chatGroups.add(chatGroup);
+                        messageAdapter = new RecyclerView_Consultant_Message(chatGroups, getContext());
+                        messageRecyclerView.setAdapter(messageAdapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        messageAdapter = new RecyclerView_Consultant_Message(chatGroups, getContext());
         messageRecyclerView.setAdapter(messageAdapter);
 
         messageRecyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        linearLayoutManager.setStackFromEnd(true);
         messageRecyclerView.setLayoutManager(linearLayoutManager);
     return view;
     }
